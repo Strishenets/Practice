33204def6c6560cc2cd2d18e05880d62500d9bd2
Strishenets <anhellina2000@gmail.com>
2024-07-02 14:54:37 +0300

    Added new project

---
+++ OOP17/OOP17/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace OOP17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(581, 581);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(631, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Зберегти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(631, 254);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 67);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Загрузити";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(631, 421);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 67);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Зберегти";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(631, 511);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 67);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Загрузити";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1126, 511);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 67);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Вихід";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(863, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 67);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "Рефлексія";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(621, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(799, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Результат:   ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(648, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "XML";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(645, 377);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Binary";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 627);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Правильний п\'ятикутник";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OOP17/OOP17/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Reflection;
+
+namespace OOP17
+{
+    public partial class Form1 : Form
+    {
+        Pentagon pentagon;
+        Graphics graphics;
+        Pen pen;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            graphics = pictureBox1.CreateGraphics();
+            pen = new Pen(Color.Black, 2);
+            pentagon = new Pentagon();
+
+            textBox1.TextChanged += TextBox1_TextChanged;
+        }
+
+        [Serializable]
+        public class Pentagon
+        {
+            public float r { get; set; }
+
+            public Pentagon()
+            {
+                r = 0;
+            }
+
+            public Pentagon(float r)
+            {
+                if (r < 1.0f)
+                    throw new ArgumentException("Радіус повинен бути не менше 1.0", nameof(r));
+
+                this.r = r;
+            }
+
+            public void Draw(Graphics graphics, Pen pen, int centerX, int centerY)
+            {
+                if (r == 0)
+                    return;
+
+                double side = 2 * r * 0.587;
+                double angle = 1.2566;
+
+                PointF[] points = new PointF[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    points[i] = new PointF(
+                        centerX + (float)(r * Math.Cos(i * angle)),
+                        centerY + (float)(r * Math.Sin(i * angle))
+                    );
+                }
+
+                graphics.Clear(Color.White);
+                graphics.DrawPolygon(pen, points);
+            }
+
+            public string Reflection()
+            {
+                Type t = typeof(Pentagon);
+                string rez = "";
+
+                MethodInfo[] x = t.GetMethods();
+                foreach (MethodInfo m in x)
+                {
+                    rez = rez + m.ToString() + "\n";
+                }
+
+                return rez;
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            foreach (char i in textBox1.Text)
+            {
+                if (!Char.IsDigit(i))
+                    return;
+            }
+
+            if (textBox1.Text != "")
+            {
+                pentagon.r = Convert.ToInt32(textBox1.Text);
+                int centerX = pictureBox1.Width / 2;
+                int centerY = pictureBox1.Height / 2;
+                pentagon.Draw(graphics, pen, centerX, centerY);
+            }
+        }
+
+        private void LoadSerializablePent()
+        {
+            if (File.Exists("Pentagon.xml"))
+            {
+                var xmlSerializer = new XmlSerializer(typeof(Pentagon));
+
+                var fs = new FileStream("Pentagon.xml", FileMode.Open);
+
+                pentagon = (Pentagon)xmlSerializer.Deserialize(fs);
+                fs.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Pentagon));
+            using (var fs = new FileStream("Pentagon.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, pentagon);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var xmlSerializer = new XmlSerializer(typeof(Pentagon));
+            if (File.Exists("Pentagon.xml"))
+            {
+                using (var fs = new FileStream("Pentagon.xml", FileMode.Open))
+                {
+                    pentagon = (Pentagon)xmlSerializer.Deserialize(fs);
+                }
+            }
+            else
+            {
+                pentagon = new Pentagon(); 
+            }
+
+            graphics.Clear(Color.White);
+            textBox1.Text = pentagon.r.ToString();
+
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+            pentagon.Draw(graphics, pen, centerX, centerY);
+        }
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var binFormatter = new BinaryFormatter();
+            using (var fs = new FileStream("Pentagon.dat", FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(fs, pentagon);
+            }
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("Pentagon.dat"))
+            {
+                var binFormatter = new BinaryFormatter();
+                using (var fs = new FileStream("Pentagon.dat", FileMode.Open))
+                {
+                    pentagon = (Pentagon)binFormatter.Deserialize(fs);
+                }
+            }
+            else
+            {
+                pentagon = new Pentagon(); 
+            }
+
+            graphics.Clear(Color.White);
+            textBox1.Text = pentagon.r.ToString();
+
+            int centerX = pictureBox1.Width / 2;
+            int centerY = pictureBox1.Height / 2;
+            pentagon.Draw(graphics, pen, centerX, centerY);
+        }
+
+
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            label1.Text = label1.Text + pentagon.Reflection();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
